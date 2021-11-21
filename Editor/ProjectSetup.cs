4cdd3d8cd0c4d6c1b2c11f057b9179881a5518b4
dhumill <dhumildholiya97@gmail.com>
2021-11-21 12:05:58 +0530

    Initial Commit

---
+++ Editor/ProjectSetup.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace BooBooTool.Editor
+{
+    public static class ProjectSetup
+    {
+        [MenuItem("BooBoo Tools/Setup/Create Default Folders", priority = 0)]
+        public static void SetupProject()
+        {
+            CreateFolderStructure("_Project", new[] {"Scripts", "Art", "Prefabs", "Scenes"});
+            SetupSceneFile();
+        }
+
+        [MenuItem("BooBoo Tools/Setup/Update Scene Items", priority = 1)]
+        public static void SetupSceneFile()
+        {
+            var dirPath = Path.Combine(Application.dataPath, "_Project/Editor/Generated");
+            var filePath = Path.Combine(dirPath, "GeneratedMenuItems.cs");
+            const string folderToSearch = "Assets/_Project/Scenes";
+
+            var guids = AssetDatabase.FindAssets("", new[] {folderToSearch});
+            var sceneNames = new List<string>();
+            var scenePaths = new List<string>();
+            foreach (var guid in guids)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                var pathSubStrings = path.Split('/');
+                scenePaths.Add(path);
+                sceneNames.Add(pathSubStrings[pathSubStrings.Length - 1].Split('.')[0]);
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("// This class is Auto-Generated");
+            sb.AppendLine("using UnityEngine;");
+            sb.AppendLine("using UnityEditor;");
+            sb.AppendLine("using UnityEditor.SceneManagement;");
+            sb.AppendLine("");
+            sb.AppendLine("  public static class GeneratedMenuItems {");
+            sb.AppendLine("");
+
+            // loops though the array and generates the menu items
+            for (var i = 0; i < sceneNames.Count; i++)
+            {
+                sb.AppendLine($"    [MenuItem(\"BooBoo Tools/Scenes/{sceneNames[i]}\")]");
+                sb.AppendLine($"    private static void MenuItem{i}()");
+                sb.AppendLine("     {");
+                sb.AppendLine($"        EditorSceneManager.OpenScene(\"{scenePaths[i]}\");");
+                sb.AppendLine("     }");
+                sb.AppendLine("");
+            }
+
+            sb.AppendLine("");
+            sb.AppendLine("}");
+
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+
+            // writes the class and imports it so it is visible in the Project window
+            File.Delete(filePath);
+            File.WriteAllText(filePath, sb.ToString(), Encoding.UTF8);
+
+            AssetDatabase.ImportAsset(Path.Combine("Assets", dirPath));
+            AssetDatabase.Refresh();
+        }
+
+        private static void CreateFolderStructure(string root, params string[] children)
+        {
+            string parentFolderPath = Path.Combine(Application.dataPath, root);
+            foreach (var child in children)
+            {
+                var path = Path.Combine(parentFolderPath, child);
+                Directory.CreateDirectory(path);
+            }
+
+            AssetDatabase.Refresh();
+        }
+    }
+}
